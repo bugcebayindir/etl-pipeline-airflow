a272f805ddbdeef0c4772b50da8e8a0cc36513d7
bugcebayindir <abugcemasaci@gmail.com>
2023-05-08 10:37:37 +0200

    adding dags file

---
+++ airflow-dags/pipeline.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+from src.main import main
+
+
+dag = DAG(dag_id="etl-pipeline",
+          description="Extracts Transforms and Loads Data",
+          schedule_interval="@daily",
+          start_date=datetime(2023,1,1),
+          catchup=False)
+
+start_dag = DummyOperator(task_id="start_dag",
+                          dag=dag)
+
+etl_operator = PythonOperator(task_id="extract_transform_load_data",
+                              python_callable=main,
+                              dag=dag)
+
+end_dag = DummyOperator(task_id="end_dag",
+                        dag=dag)
+
+start_dag >> etl_operator >> end_dag
